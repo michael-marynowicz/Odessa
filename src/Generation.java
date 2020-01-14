c8e26dd61945f20c5d504c18c6a5811e5c90e8a4
Zorrino <41651765+Zorrino@users.noreply.github.com>
2020-01-14 16:44:43 +0100

    generation matrice 20x20 et salle 6x6 avec un pattern

--- src/Generation.java
+++ src/Generation.java
@@ -1,59 +1,26 @@
 package src;
 
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Generation {
 
     public static void main(String[] args) {
-        Fenetre fen = new Fenetre();
-        Points[][] tableau = new Points[11][11];
-        Points Origine = new Points(5, 5, 1);
-        tableau[5][5] = Origine;
-        ArrayList<Points> PointsEnCours = new ArrayList<>();
-    }
-
-    void execution(ArrayList<Points> PointsEnCours, Points[][] tableau) {
-        ArrayList<Points> PointsAjouter = new ArrayList<Points>();
-        int incrementeurPointMort = 0;
-        for (int i = 0; i < PointsEnCours.size(); i++) {
-
-            Points points_up = new Points(PointsEnCours.get(i).getX(), PointsEnCours.get(i).getY() - 1, 1);
-            Points points_down = new Points(PointsEnCours.get(i).getX(), PointsEnCours.get(i).getY() + 1, 1);
-            Points points_right = new Points(PointsEnCours.get(i).getX() + 1, PointsEnCours.get(i).getY(), 1);
-            Points points_left = new Points(PointsEnCours.get(i).getX() - 1, PointsEnCours.get(i).getY(), 1);
-            if (tableau[points_up.getX()][points_up.getY()] != null) {
-                tableau[points_up.getX()][points_up.getY()] = points_up;
-                PointsAjouter.add(points_up);
+        int[][] tableau = new int[20][20];
+        for(int i = 0; i< tableau.length; i++){
+            for(int j = 0; j< tableau.length; j++){
+                tableau[i][j] = 0;
+                if((j+1) == tableau.length){
+                    System.out.print(tableau[i][j]);
+                }
+                else {
+                    System.out.print(tableau[i][j] + "   ");
+                }
             }
-            if (tableau[points_down.getX()][points_down.getY()] != null) {
-                tableau[points_down.getX()][points_down.getY()] = points_down;
-                PointsAjouter.add(points_down);
-            }
-            if (tableau[points_right.getX()][points_right.getY()] != null) {
-                tableau[points_right.getX()][points_right.getY()] = points_right;
-                PointsAjouter.add(points_right);
-            }
-            if (tableau[points_left.getX()][points_left.getY()] != null) {
-                tableau[points_left.getX()][points_left.getY()] = points_left;
-                PointsAjouter.add(points_left);
-            }
-
-            incrementeurPointMort += 2;
+            System.out.print('\n');
         }
 
-        PointsEnCours = (ArrayList<Points>) PointsAjouter.clone();
+        Salle test = new Salle();
+        test.getTableau();
     }
+
 }
-
-
-
-/*
-    public class Generation {
-        Random r = new Random();
-        Points Origine = new Points(100, 100, true);
-        ArrayList<Points> PointsEnCours = new ArrayList<>();
-
-
-    } */
