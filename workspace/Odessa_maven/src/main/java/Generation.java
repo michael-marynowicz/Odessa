9d185763dfee8f0b6027c13ed03ead87f6596b3f
Zorrino <41651765+Zorrino@users.noreply.github.com>
2020-08-25 18:01:44 +0200

    Implementation de maven

---
+++ workspace/Odessa_maven/src/main/java/Generation.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Generation {
+    public static void main(String[] args) throws InterruptedException {
+        /* Le fait qu'on génère le donjon dans le main peut être chiant pour la suite si on imagine par exemple
+        créer différents étages qui pourraient avoir différentes façon de se générer, différentes tailles ou des types.
+        On peut voir si on le met dans une classe à part ou pas.
+         */
+        Pattern.genPattern();
+        Donjon matriceDonjon = new Donjon(20,20);
+
+        Salle salle1 = new Salle();
+        Salle salle2 = new Salle();
+        Salle salle3 = new Salle();
+        Salle salle4 = new Salle();
+        Salle salle5 = new Salle();
+        Salle salle6 = new Salle();
+        salle1.genSalle();
+        salle1.printSalle();
+
+        matriceDonjon.ajoutSalle(salle1);
+        matriceDonjon.ajoutSalle(salle2);
+        matriceDonjon.ajoutSalle(salle3);
+        matriceDonjon.ajoutSalle(salle4);
+        matriceDonjon.ajoutSalle(salle5);
+        matriceDonjon.ajoutSalle(salle6);
+        matriceDonjon.printDonjon();
+
+
+
+    }
+
+}
