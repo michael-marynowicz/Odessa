33d479448f43e4d33fd37aab3c1e029896c2843c
michael-marynowicz <79304440+michael-marynowicz@users.noreply.github.com>
2021-02-27 18:04:21 +0100

    Add files via upload
    couloir

--- workspace/Odessa_maven/src/main/java/Case.java
+++ workspace/Odessa_maven/src/main/java/Case.java
@@ -1,20 +1,27 @@
-package main.java;
+
 
 public class Case {
 
     private boolean isDoor;
     public int printConsole = 1;
+
+
     private int coordonneeY;
     private int coordonneeX;
-
+    private Case parent = null;
     private Case(){}
+
+    Case(int x, int y){
+        this.coordonneeX = x;
+        this.coordonneeY = y;
+    }
 
     Case(boolean isDoor){
         this.isDoor = isDoor;
     }
 
     Case(int vide){
-        this.printConsole = 0;
+        this.printConsole = vide;
     }
 
     Case(boolean isDoor, int porte){ //Sert à mettre un 2 dans la console (c'est plus lisible lol)
@@ -24,4 +31,22 @@ public class Case {
 
     public boolean isDoor() { return isDoor; }
     public int getPrintConsole(){ return this.printConsole; }
+
+    public int getCoordonneeY() {
+        return coordonneeY;
+    }
+
+    public int getCoordonneeX() {
+        return coordonneeX;
+    }
+
+    public Case getParent() {
+        return parent;
+    }
+
+    public void setParent(Case parent) {
+        this.parent = parent;
+    }
 }
+
+
